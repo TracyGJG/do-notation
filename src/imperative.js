fb7b8e30f9763bd8a8f20abeb5c98b1369ef68d0
TGJ Gilmore <TracyGJG@users.noreply.github.com>
2023-05-28 12:47:47 +0100

    Realigned and page revised

--- src/imperative.js
+++ src/imperative.js
@@ -1,50 +1,23 @@
-<<<<<<< HEAD
 import runTestCases from './testcases.js';
 
-=======
->>>>>>> f538714 ("Initial commit")
 function add(m, n) {
-  return m + n;
+	return m + n;
 }
 function sub(m, n) {
-  return m - n;
+	return m - n;
 }
 function mul(m, n) {
-  return m * n;
+	return m * n;
 }
 function div(m, n) {
-  return m / n;
+	return m / n;
 }
 
 function cToF(c) {
-  return add(div(mul(c, 9), 5), 32);
+	return add(div(mul(c, 9), 5), 32);
 }
 function fToC(f) {
-  return div(mul(sub(f, 32), 5), 9);
+	return div(mul(sub(f, 32), 5), 9);
 }
 
-<<<<<<< HEAD
 runTestCases(cToF, fToC);
-=======
-const testCases = [
-  { name: "cToF", func: cToF, input: 100, expected: 212 },
-  { name: "cToF", func: cToF, input: 0, expected: 32 },
-  { name: "cToF", func: cToF, input: -40, expected: -40 },
-  { name: "fToC", func: fToC, input: 212, expected: 100 },
-  { name: "fToC", func: fToC, input: 32, expected: 0 },
-  { name: "fToC", func: fToC, input: -40, expected: -40 },
-];
-
-console.table(
-  testCases.map(({ name, func, input, expected }) => {
-    const actual = func(input);
-    const result = actual === expected;
-    return {
-      "Test case": `${name} of ${input}`,
-      expected,
-      actual,
-      result,
-    };
-  })
-);
->>>>>>> f538714 ("Initial commit")
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import "./styles.css";
+import './styles.css';
 
-document.getElementById("app").innerHTML = ``;
+document.getElementById('app').innerHTML = `
+<main>
+    <h1>Simulating Haskell's Do Notation in JS</h1>
+    <h2>Usecase: Celsius/Fahrenheit temporature conversion</h2>
+</main>`;
